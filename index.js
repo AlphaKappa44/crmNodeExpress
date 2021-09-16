4ed622031549619e1aafc1d9c491e77547487cea
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-09-16 12:52:56 +0200

    tested routes ok in crmRoutes.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // import express from 'express';
-// const route = require('./src/routes/crmRoutes.js');
+const route = require('./src/routes/crmRoutes.js');
 // import routes from './src/routes/crmRoutes';
 
 // routes(app);
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 const PORT = 4445;
 
-// app.use(route);
+app.use(route);
 
 app.get('/', (req, res) =>
     res.send(`Serveur node.js et express.js sur le trèèès fameux port ${PORT}`)
@@ -18,18 +18,4 @@ app.listen(PORT, () =>
     console.log(`Your server was launched at http://localhost:${PORT}`)
     );
 
-app.get('/contact', (req, res) =>
-    res.send('La requète "GET /contact" est trouvée avec succès!')
-    );
-app.post('/contact', (req, res) =>
-    res.send('La requète "POST /contact" est envoyée avec succès!')
-    );
-
-app.put('/contact/:contactId', (req, res) =>
-    res.send('La requète "PUT /contact/:contactId" a modifié avec succès!')
-    )
-
-app.delete('/contact/:contactId', (req, res) =>
-    res.send('La requète "DELETE /contact/:contactId" a supprimé avec succès!')
-    );
 
